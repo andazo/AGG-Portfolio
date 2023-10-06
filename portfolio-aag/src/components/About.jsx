4cb431f27603b77d2f60cc254f3a5c70ba568557
Andres-Azofeifa <andazo2501@gmail.com>
2023-10-06 17:49:27 -0600

    Format changes to Navbar and About component

--- portfolio-aag/src/components/About.jsx
+++ portfolio-aag/src/components/About.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-[#8892b0]'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-5xl text-[#ccd6f6] font-bold inline border-b-4 border-pink-600'>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-[#8892b0]">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-5xl text-[#ccd6f6] font-bold inline border-b-4 border-pink-600">
               About
             </p>
           </div>
           <div></div>
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Andrés Azofeifa García, nice to meet you. Take a look around and don't hesitate to contact me.</p>
+        </div>
+        <div className="max-w-[1170px] w-full grid sm:grid-cols-2 gap-8 px-4">
+            <div className="sm:text-right text-4xl font-bold">
+                <p>
+                    Nice to meet you. Take a look around and don't hesitate to contact
+                    me.
+                </p>
             </div>
-            <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
-            </div>
+          <div>
+            <p className="font-bold text-lg">
+              I am passionate software developer with an aptitude for crafting
+              exceptional and inclusive web experiences. When I'm not immersed
+              in the world of software and computers, you'll find me exploring my diverse
+              interests, from sports to space, design to engineering. I cherish
+              quality time with my dog, family, and close friends.
+            </p>
           </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
